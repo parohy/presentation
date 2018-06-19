b18af5f6e6fa7b7fad50415fd4d14972dbb8d212
Tomas Paronai <tomas.paronai@globallogic.com>
2018-06-19 12:17:25 +0200

    1. Change welcome text color and size. 2. Change instructions marginTop and color.

--- App.js
+++ App.js
@@ -33,13 +33,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF'
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 25,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
+    color: 'blue'
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: 'black',
+    marginTop: 15,
     marginBottom: 5
   }
 });
